ea0b5858d0346452aa2992f846d8b4c0421bacba
Inha Povarova <inha.povarova@hm.edu>
2026-07-05 13:05:33 +0200

    Feat: Added optional process env port for hosting

--- project/server.js
+++ project/server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const generatedDir = path.join(__dirname, "assets", "generated-buildings");
 const DEFAULT_AESTHETIC = "neutral";
